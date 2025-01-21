6293ff7cdfc38f3454a352b3665cd8226e6a2995
Nathan Kau <nathankau@gmail.com>
2025-01-21 14:42:45 -0800

    add build script

---
+++ ros2_ws/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source /opt/ros/jazzy/setup.bash
+
+cd "$SCRIPT_DIR" || exit
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+source "$SCRIPT_DIR/install/local_setup.bash"
