26f44ae4078570b339ca89e765e25e1a2d45c2a7
Nathan Kau <nathankau@gmamil.com>
2025-02-02 15:35:08 -0800

    update autostart

--- robot/utils/robot.sh
+++ robot/utils/robot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /opt/ros/jazzy/setup.bash
-source /home/pi/ros2_ws/install/setup.bash
+source /home/pi/pupperv3-monorepo/ros2_ws/install/local_setup.bash
 ros2 launch neural_controller launch.py &
 ros2 run pupper_feelings face_control
