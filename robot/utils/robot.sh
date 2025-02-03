86113684c31a6d145b041e2c76612cbf0b4c834f
Nathan Kau <nathankau@gmamil.com>
2025-02-02 20:06:20 -0800

    don't start face control

--- robot/utils/robot.sh
+++ robot/utils/robot.sh
@@ -2,5 +2,5 @@
 
 source /opt/ros/jazzy/setup.bash
 source /home/pi/pupperv3-monorepo/ros2_ws/install/local_setup.bash
-ros2 launch neural_controller launch.py &
-ros2 run pupper_feelings face_control
+ros2 launch neural_controller launch.py
+# ros2 run pupper_feelings face_control
