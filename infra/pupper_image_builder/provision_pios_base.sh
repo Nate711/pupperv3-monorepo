917b178f6e10130a039a5350d2ed8109c4700af5
Nathan Kau <nathankau@gmamil.com>
2025-02-09 14:43:44 -0800

    add back sound overlay!

--- infra/pupper_image_builder/provision_pios_base.sh
+++ infra/pupper_image_builder/provision_pios_base.sh
@@ -57,7 +57,7 @@ echo 'dtparam=spi=on' | sudo tee -a /boot/firmware/config.txt
 echo 'dtparam=i2c_arm=on,i2c_arm_baudrate=100000' | sudo tee -a /boot/firmware/config.txt
 echo 'usb_max_current_enable=1' | sudo tee -a /boot/firmware/config.txt
 
-# At the bottom of config.txt
+# Set up touch screen and HDMI settings
 echo 'dtoverlay=waveshare-4dpic-3b' >> /boot/firmware/config.txt
 echo 'dtoverlay=waveshare-4dpic-4b' >> /boot/firmware/config.txt
 echo 'dtoverlay=waveshare-4dpic-5b' >> /boot/firmware/config.txt
@@ -68,8 +68,13 @@ echo 'hdmi_mode=87' >> /boot/firmware/config.txt
 echo 'hdmi_timings=720 0 100 20 100 720 0 20 8 20 0 0 0 60 0 48000000 6' >> /boot/firmware/config.txt
 echo 'start_x=0' >> /boot/firmware/config.txt
 echo 'gpu_mem=128' >> /boot/firmware/config.txt
-echo 'dtoverlay=hifiberry-dac' >> /boot/firmware/config.txt
+
+# Try to set rotation settings
 sed -i '1s/^/video=HDMI-A-1:720x720M@60D,rotate=270 /' /boot/firmware/cmdline.txt
+
+# Enable speaker
+echo 'dtoverlay=hifiberry-dac' >> /boot/firmware/config.txt
+
 
 # Set up firstboot script
 # NOT WORKING RIGHT NOW
