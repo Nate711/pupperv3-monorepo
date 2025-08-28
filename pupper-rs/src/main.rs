337ef0cc1e69d4ae9ec06f48afb768a5237d8a57
Nathan Kau <nathankau@gmail.com>
2025-08-28 12:06:48 -0700

    add rust crate for UI

--- pupper-rs/src/main.rs
+++ pupper-rs/src/main.rs
@@ -1,34 +1,36 @@
-use eframe::{egui, App};
+use eframe::{App, egui};
 use egui_extras::image::load_image_bytes;
 use std::path::PathBuf;
 
 struct ImageApp {
-    texture: egui::TextureHandle,
-    size: egui::Vec2,
+    // texture: egui::TextureHandle,
+    // size: egui::Vec2,
 }
 
 impl ImageApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let image_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-            .join("..")
-            .join("infra")
-            .join("pupper_image_builder")
-            .join("resources")
-            .join("blue_eyes.jpg");
-        let bytes = std::fs::read(image_path).expect("failed to read blue_eyes.jpg");
-        let image = load_image_bytes(&bytes).expect("invalid image data");
-        let size = egui::Vec2::new(image.width() as f32, image.height() as f32);
-        let texture =
-            cc.egui_ctx
-                .load_texture("blue_eyes", image, egui::TextureOptions::default());
-        Self { texture, size }
+        egui_extras::install_image_loaders(&cc.egui_ctx);
+        // let image_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        //     .join("..")
+        //     .join("infra")
+        //     .join("pupper_image_builder")
+        //     .join("resources")
+        //     .join("blue_eyes.jpg");
+        // let bytes = std::fs::read(image_path).expect("failed to read blue_eyes.jpg");
+        // let image = load_image_bytes(&bytes).expect("invalid image data");
+        // let size = egui::Vec2::new(image.width() as f32, image.height() as f32);
+        // let texture = cc
+        //     .egui_ctx
+        //     .load_texture("blue_eyes", image, egui::TextureOptions::default());
+        // Self { texture, size }
+        Self {}
     }
 }
 
 impl App for ImageApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.image((self.texture.id(), self.size));
+            ui.image(egui::include_image!("blue_eyes.png"));
         });
     }
 }
@@ -44,4 +46,3 @@ fn main() -> eframe::Result<()> {
         Box::new(|cc| Box::new(ImageApp::new(cc))),
     )
 }
-
