170b8ffccf2025388d6408ad43b92050917e7051
Nathan Kau <nathankau@gmamil.com>
2025-09-01 17:04:18 -0700

    set vol to 100% before running llm agent

--- ai/llm-ui/agent-starter-python/run.sh
+++ ai/llm-ui/agent-starter-python/run.sh
@@ -3,6 +3,9 @@
 # Source ROS2 environment
 source /opt/ros/jazzy/setup.bash
 
+# Set vol to 100%
+amixer sset 'Master' 100%
+
 # Source local workspace
 source /home/pi/pupperv3-monorepo/ros2_ws/install/local_setup.bash
 
