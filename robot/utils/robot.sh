43112cdf2d039a1a2ed6ece4289b1f01a41950c7
Nathan Kau <nathankau@gmail.com>
2025-09-17 14:18:06 -0700

    set vol to 100% when robot stack starts

--- robot/utils/robot.sh
+++ robot/utils/robot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+amixer sset 'Master' 100%
 source /opt/ros/jazzy/setup.bash
 source /home/pi/pupperv3-monorepo/ros2_ws/install/local_setup.bash
 ros2 launch neural_controller launch.py
-# ros2 run pupper_feelings face_control
+
