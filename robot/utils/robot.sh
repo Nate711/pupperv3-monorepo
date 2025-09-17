2f944fe8901449f903fe33cf3f552d22a5daee3d
Nathan Kau <nathankau@gmamil.com>
2025-09-17 15:06:34 -0700

    refactor: remove unnecessary sound target dependency and volume setting from service and script

--- robot/utils/robot.sh
+++ robot/utils/robot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-amixer sset 'Master' 100%
 source /opt/ros/jazzy/setup.bash
 source /home/pi/pupperv3-monorepo/ros2_ws/install/local_setup.bash
 ros2 launch neural_controller launch.py
